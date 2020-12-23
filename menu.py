df300a22a6bb60f1b700368c8d13406727f3eed1
proect123 <123proect123@gmail.com>
2020-12-23 18:51:57 +1000

    -

--- menu.py
+++ menu.py
@@ -1,8 +1,2 @@
 import pygame
-from main import screen_size, screen
-
-pygame.init()
-
-def show_menu():
-    screen = pygame.display.set_mode((screen_size[0] // 2, screen_size[1] // 2))
-    screen_size = ((screen_size[0] // 2, screen_size[1] // 2))
+from settings import *
---
+++ settings.py
@@ -0,0 +1,4 @@
+import pygame as pg
+
+screen_size = screen_width, screen_height = (1920, 1080)
+screen = pg.display.set_mode(screen_size)
