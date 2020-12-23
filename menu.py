e5ff04a079c3ea37007d4e3f621ea52cd289a4fa
proect123 <123proect123@gmail.com>
2020-12-23 19:15:04 +1000

    -

--- menu.py
+++ menu.py
@@ -1,9 +1,15 @@
-import pygame as pg
 from settings import *
+
+button_image = pg.image.load('textures/button.png')
+button_pressed_image = pg.image.load('textures/button_pressed.png')
+
 
 def show_menu():
     screen.fill((100, 100, 100))
     
+    screen.blit(button_image, (0,0))
+    screen.blit(button_pressed_image, (100,0))
+
     pg.display.update()
 
 show_menu()
