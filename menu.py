9d5d43aab90d22d95ad2f77b48dd5f9dd5613f79
proect123 <123proect123@gmail.com>
2020-12-23 23:25:35 +1000

    -

--- menu.py
+++ menu.py
@@ -1,9 +1,9 @@
-from settings import screen, screen_size, pg, best_font
+from settings import screen, screen_size, screen_width, screen_height, pg, best_font
 
-button_image = pg.image.load('textures/button.png')
+button_image = pg.image.load('proect/textures/button.png')
 button_rect = button_image.get_rect()
 
-bg_image = pg.image.load('textures/bg_image.png')
+bg_image = pg.image.load('proect/textures/bg_image.png')
 
 mouse_rect = pg.Rect((0, 0, 1, 1))
 
@@ -11,12 +11,30 @@ class Button():
     def __init__(self, x, y):
         self.rect = pg.Rect((x, y, button_rect.width, button_rect.height))
         self.image = button_image
+        self.text = 'heelo'
+
+    def update(self):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+        screen.blit(best_font.render(self.text, True, (0, 0, 0)),\
+                    (self.rect.x + self.rect.width // 2 - best_font.size(self.text)[0] // 2,\
+                    self.rect.y + self.rect.height // 2 - best_font.size(self.text)[1] // 2))
+
+button_list = []
+for i in range(3):
+    button_list.append(Button(screen_width - button_rect.width - 50, screen_height - 100 - i * (button_rect.height + 10)))
+button_list[-1].text = 'START'
+button_list[-2].text = 'SETTINGS'
+button_list[-3].text = 'QUIT'
 
 
 def show_menu():
-    screen.blit(bg_image, (0, 0))
+    while 1:
+        screen.blit(bg_image, (0, 0))
 
-    pg.display.update()
+        for btn in button_list:
+            btn.update()
+
+        pg.display.update()
 
 show_menu()
 
--- settings.py
+++ settings.py
@@ -4,3 +4,5 @@ pg.init()
 
 screen_size = screen_width, screen_height = (1920, 1080)
 screen = pg.display.set_mode(screen_size)
+
+best_font = pg.font.SysFont('Bebas Neue', 50)
