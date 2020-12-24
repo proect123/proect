5826b4c27f6fc9b7d451520a8173c16cd4824379
proect123 <123proect123@gmail.com>
2020-12-24 15:56:29 +1000

    -

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ pg.init()
 screen_size = screen_width, screen_height = (1920, 1080)
 screen = pg.display.set_mode(screen_size)
 
-best_font = pg.font.SysFont('Bebas Neue', 50)
+best_font = pg.font.Font('proect/BebasNeue.ttf', 50)
